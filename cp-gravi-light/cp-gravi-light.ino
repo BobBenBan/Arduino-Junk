e5590df5bba9d1f88938328a4b001a1cb33b4093
Benjamin Ye <anemailofben@gmail.com>
2017-04-24 21:57:21 -0700

    gravi-light progress

--- cp-gravi-light/cp-gravi-light.ino
+++ cp-gravi-light/cp-gravi-light.ino
@@ -1,57 +1,69 @@
+#include <Adafruit_NeoPixel.h>
 #include <Adafruit_CircuitPlayground.h>
-#define cp CircuitPlayground
+
+#define PIN 17
+#define builtInRing
+#ifdef builtInRing
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(10, 17);
+#else
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(12, PIN);
+#endif
 uint8_t const trailmin = 1;
-uint8_t const trailmax = 10;
-void sp(uint8_t p, uint8_t r, uint8_t g, uint8_t b) {
-  cp.setPixelColor(p, r,g,b);
+uint8_t const trailmax = 140;
+uint8_t const trailstp = trailmax/15;
+uint8_t const trailhlt = 20;
+void sp(uint8_t p, uint32_t c) {
+#ifdef builtInRing
+  if (p == 5 || p == 11)return;
+  if (p > 5)p--;
+#endif
+  ring.setPixelColor(p, c);
 }
+float rpix;
 uint8_t accelPix() {
-  float rpix = 8 - atan2(cp.motionX(), cp.motionY()) / 3.141592654 * 6;
-  if (rpix >= 12)rpix -= 12;
-  uint8_t pix = round(rpix) % 12;
+  rpix = 8 - atan2(CircuitPlayground.motionX(), CircuitPlayground.motionY()) / 3.141592654 * 6;
+  if (rpix > 12)rpix -= 12;
+  return round(rpix) % 12;
+}
+uint32_t wheel(uint8_t p, uint8_t t) {
+  uint8_t v = 255 * t;
+  if (p < 3) return ring.Color(t, 0, 0);
+  if (p < 6) return ring.Color(0, 0, t);
+  if (p < 9) return ring.Color(0, t, 0);
+  return ring.Color(t, t, 0);
+}
+int16_t trail[12];
+void showPix(uint8_t pix) {
+  uint8_t prevpix = 0;
+#ifdef builtInRing
   if (pix == 5)
-    pix = (rpix > 5) ? 5 : 4;
+    pix = (rpix > 5) ? 6 : 4;
   else if (pix == 11)
-    pix = (rpix > 11) ? 0 : 9;
-  else if (pix > 5)pix--;
-  return pix;
+    pix = (rpix > 11) ? 0 : 10;
+#endif
+  uint8_t h = (pix / 3) * 3;
+  for (uint8_t i = 0; i < 3; i++) {
+    if (trail[h + i] < trailhlt) trail[h + i] = trailhlt;
+  }
+  trail[pix] = trailmax;
+  for (uint8_t i = 0; i < 12; i++) {
+    sp(i, wheel(i, trail[i])) ;
+    if (trail[i] > trailmin) trail[i] -= trailstp;
+    if (trail[i] < trailmin) trail[i] = trailmin;
+  }
+  ring.show();
 }
-void showWheel(uint8_t p, uint8_t t) {
-  uint8_t c[] = {2, 3, 4, 5, 6, 8, 9, 10, 11, 0};
-  int l = c[p];
-  if (l < 3) {
-    sp(p, 85 * l * t / trailmax, 255 * t / trailmax, 0); return;
-  }
-  l -= 3;
-  if (l < 3) {
-    sp(p, (255 - 85 * l) * t / trailmax, (255 - 85 * l) * t / trailmax, 85 * l * t / trailmax); return;
-  }
-  l -= 3;
-  if (l < 3) {
-    sp(p, 85 * l * t / trailmax, 0, (255 - 85 * l) * t / trailmax); return;
-  }
-  l -= 3;
-  sp(p, (255 - 85 * l) * t / trailmax, 85 * l * t / trailmax, 0);
-}
-uint8_t trail[10];
-void showPix() {
-  for (uint8_t i = 0; i < 10; i++) {
-    showWheel(i, trail[i]);
-    if (trail[i] > trailmin)trail[i]--;
-  }
-}
-uint8_t prevpix = 0;
 void setup() {
-  cp.begin();
-  Serial.begin(9600);
-  while (!Serial);
-  memset(&trail, trailmin, 10);
+  CircuitPlayground.begin();
+  ring.begin();
+  /*/
+    Serial.begin(9600);
+    while (!Serial);//*/
+  memset(&trail, trailmin, 12);
 }
 void loop() {
   uint8_t pix = accelPix();
-  trail[pix] = trailmax;
-  prevpix = pix;
-  showPix();
+  showPix(pix);
   delay(10);
 }
 
