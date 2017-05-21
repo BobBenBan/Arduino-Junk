e3fbbedfd94eae3318d6a32c5d8b8fcf95c6ee6e
Benjamin Ye <anemailofben@gmail.com>
2017-05-21 11:04:11 -0700

    Weather

--- weather_station_color/settings.h
+++ weather_station_color/settings.h
@@ -22,7 +22,7 @@ See more at http://blog.squix.ch
 const int UPDATE_INTERVAL_SECS = 10 * 60;  // Update every 10 minutes
 boolean USE_TOUCHSCREEN_WAKE = true;       // use the touchscreen to wake up, ~90mA current draw
 boolean DEEP_SLEEP = false;                 // use the touchscreen for deep sleep, ~10mA current draw but doesnt work
-int     AWAKE_TIME = 10;                   // how many seconds to stay 'awake' before going back to zzz
+int     AWAKE_TIME = 5;                   // how many seconds to stay 'awake' before going back to zzz
 
 // Pins for the ILI9341
 #define TFT_DC 15
@@ -37,7 +37,7 @@ const float UTC_OFFSET = -5;
 
 // Wunderground Settings
 const boolean IS_METRIC = false;
-const String WUNDERGRROUND_API_KEY = "4fdb07ec0cad8c0f";
+const String WUNDERGRROUND_API_KEY = "ccc9c3f73c3b7987";
 const String WUNDERGRROUND_LANGUAGE = "EN";
 const String WUNDERGROUND_COUNTRY = "US";
 const String WUNDERGROUND_CITY = "CA/Fremont";
@@ -52,3 +52,4 @@ String wundergroundIcons [] = {"chanceflurries","chancerain","chancesleet","chan
 /***************************
  * End Settings
  **************************/
+
